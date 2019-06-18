87160decfa646a8e0bbd6cf6c8ffa5c16c19448b
cymheart <cymheart@qq.com>
2019-06-18 17:59:35 +0800

    ok

--- Watershed/Watershed.cs
+++ Watershed/Watershed.cs
@@ -42,7 +42,7 @@ namespace Watershed
 
         object lockObj = new object();
         List<ManualResetEvent> manualEvents = new List<ManualResetEvent>();
-        int threadCount = 60;
+        int threadCount = 300;
 
         Stack<WatershedElem> searchElemStack = new Stack<WatershedElem>();
         List<List<WatershedElem>> waterElemRegionList = new List<List<WatershedElem>>(500);
